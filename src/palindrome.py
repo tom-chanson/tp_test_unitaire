83f4659a20d6445037a766b6b9ff16e939ea7ad9
Tom Chanson <tom.chanson@epsi.fr>
2024-01-31 15:56:05 +0100

    [GREEN] langue - bien dit

--- src/palindrome.py
+++ src/palindrome.py
@@ -2,12 +2,17 @@ import os
 
 
 class Palindrome:
+
+    def __init__(self, langue):
+        self.__langue = langue
+
+
     def mirroir(self, mot):
         return mot[::-1]
     
     def palindrome(self, mot):
         resultat = "bonjour" + os.linesep + mot
         if mot == self.mirroir(mot):
-            resultat += os.linesep + "bien dit"
+            resultat += os.linesep + self.__langue.bien_dit()
         return resultat + os.linesep + "au revoir"
 
