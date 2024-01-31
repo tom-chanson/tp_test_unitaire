f31bde07c8cb460411fc8c7efd3775f31d5a4133
Tom Chanson <tom.chanson@epsi.fr>
2024-01-31 15:32:14 +0100

    [GEEN] bonjour

--- src/palindrome.py
+++ src/palindrome.py
@@ -6,7 +6,7 @@ class Palindrome:
         return mot[::-1]
     
     def palindrome(self, mot):
-        resultat = mot
+        resultat = "bonjour" + os.linesep + mot
         if mot == self.mirroir(mot):
             resultat += os.linesep + "bien dit"
         return resultat
